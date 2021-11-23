e27a41a77d4af0dca1d5a16f468f2811ec2aff27
Matteo <matteokitic7@gmail.com>
2021-11-23 18:08:22 -0500

    Script ready for use

--- search_users.py
+++ search_users.py
@@ -2,20 +2,18 @@ from psaw import PushshiftAPI
 import datetime
 import praw
 import threading
-
+import re
 
 
 api = PushshiftAPI()
-start_time = int(datetime.datetime(2019, 1, 1).timestamp())
 
+#Edit start date for searched posts here
+start_time = int(datetime.datetime(2021, 6, 25).timestamp())
+
+#Variable to store all submissions and posts
 submissions = api.search_submissions(after=start_time,
-                                    subreddit='asktrp',
-                                    filter=['url','author', 'title', 'subreddit'], limit = 100)
-
-
-comments = api.search_comments(after=start_time,
-                                    subreddit='asktrp',
-                                    filter=['url','author', 'title', 'subreddit'], limit = 100)
+                                    subreddit='newTRP',
+                                    filter=['url','author', 'title', 'subreddit'], limit = 20)
 
 #All found usernames are written to new text file Users.txt
 users = open("Users.txt","w")
@@ -26,38 +24,45 @@ with open("restrictedUsers.txt") as file:
     while (line := file.readline().rstrip()):
         blockedUsersSet.add(line)
 
-#A set to prevent duplicates from writing to the file
+#A set of users to prevent duplicates from writing to the file
 usersSet = {""}
+#A set for links to each thread
+linksSet = {""}
 
 #Searches all post authors and adds to set
 for submission in submissions:
     if submission.author not in blockedUsersSet:
         usersSet.add(submission.author)
+        linksSet.add(submission.url)
 
-#Searches all comment authors and adds to set
-for comment in comments:
-    if comment.author not in blockedUsersSet:
-        usersSet.add(comment.author)
+#Reddit API credentials
+reddit = praw.Reddit(
+    client_id="", #Insert your Id here
+    client_secret="", #Insert your secret id
+    password="", #Insert your password
+    user_agent="agent",
+    username="", #Insert your username
+)
+
+#Searches each post for comment authors and adds to set
+for link in linksSet:
+    if link != "":
+        url = link
+        submission = reddit.submission(url=url)
+        for top_level_comment in submission.comments:
+            name = str (top_level_comment.author)
+            re.findall(r"'([^']+)'", name)
+            if name not in blockedUsersSet:
+                usersSet.add(name)
+            
 
 #Writes users to text file
 for user in usersSet:
     users.writelines(user + "\n")
-'''
-#API credentials
-reddit = praw.Reddit(
-    client_id="aMDdLPzpiQ1rVTWpaCg7PQ",
-    client_secret="7BGwl2oOSgq8-js7K4Jm75cCaJtr2A",
-    password="matteo11",
-    user_agent="USERAGENT",
-    username="LifeExposed",
-)
 
-#message users
+
+#Message users
 for user in usersSet:
     if (user != ""):
-        reddit.redditor(user).message("The New r/asktrp!",
-         "**##Hello!** we have decided to start a new community at **#r/askrp** to replace r/asktrp. **Please come join us at #r/askrp** so we can have an **##active community!**")
-
-'''
-
-print("Application executed successfully")
+        reddit.redditor(user).message("Message Title Here", #Insert Message title
+         "Message body") #Insert Message Body
